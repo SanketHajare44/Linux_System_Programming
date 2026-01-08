10be20caadf080a9192df08289f00996ea47c475
SanketHajare44 <sankethajare44@gmail.com>
2026-01-08 22:39:05 +0530

    Implement pread() system call

---
+++ System_Calls/File_Subsystem_System_Calls/pread_1.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<unistd.h>
+#include<stdlib.h>
+#include<fcntl.h>
+
+
+int main()
+{
+    int fd = 0;
+
+    off_t offset = 0;
+
+    char Buffer[100];
+
+    fd = open("./LSP.txt",O_RDONLY);
+
+    offset = lseek(fd,0,SEEK_SET);
+    printf("Currrent offset is  : %ld\n",offset);      // 0
+
+    read(fd,Buffer,10);
+
+    offset = lseek(fd,0,SEEK_CUR);
+    printf("Currrent offset is  : %ld\n",offset);      // 10
+
+    return 0;
+}
