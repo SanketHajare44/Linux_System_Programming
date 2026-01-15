ba79ea7d71da70dc96dc6ca8b809d983f1ff6957
SanketHajare44 <sankethajare44@gmail.com>
2026-01-15 23:09:20 +0530

    Demonstrate stat() system call

---
+++ System_Calls/File_Subsystem_System_Calls/stat_6.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<unistd.h>
+#include<fcntl.h>
+#include<sys/stat.h>
+
+int main()
+{   
+    struct stat sobj;
+    int iRet = 0;
+
+    iRet = stat("./LSPl.txt",&sobj);
+
+    printf("Inode number : %lu\n",sobj.st_ino);
+
+    printf("Hardlink count : %lu\n",sobj.st_nlink);
+
+    printf("Total size : %lu\n",sobj.st_size);
+
+    printf("Block size: %lu\n",sobj.st_blksize);
+
+    printf("File type is : %d\n",sobj.st_mode);
+
+    return 0;
+
+
+}
