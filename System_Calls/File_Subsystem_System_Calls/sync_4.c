f65f0c208e474a354c49ac577324114933babac9
SanketHajare44 <sankethajare44@gmail.com>
2026-01-16 23:17:41 +0530

    forces all data of a specific file to be written from RAM to disk.

---
+++ System_Calls/File_Subsystem_System_Calls/sync_4.c
@@ -0,0 +1,36 @@
+#include<unistd.h>
+#include<stdio.h>
+#include<fcntl.h>
+#include<errno.h>
+#include<string.h>
+
+int main()
+{
+    int fd = 0;
+    char Buffer[] = "indiaismycountry";
+    int iRet = 0;
+
+    fd = open("Demo.txt",O_CREAT | O_WRONLY | O_APPEND, 0777);   // IMP
+
+    if(fd < 0)
+    {
+        printf("Unable to open the file \n");
+        printf("Reson : %s\n",strerror(errno));
+
+        return -1;
+
+    }
+
+    printf("File successfully opend with : %d\n",fd);
+
+    iRet = write(fd, Buffer, 16);                       // Over write issue
+
+    printf("%d bytes successfully written\n",iRet);
+
+    fsync(fd);
+    // Process is in waiting state
+
+    close(fd);
+
+    return 0;
+}
