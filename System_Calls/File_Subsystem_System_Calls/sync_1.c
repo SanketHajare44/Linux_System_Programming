9ebbb3ea6468a95aeb85904da55e4599b90c4d21
SanketHajare44 <sankethajare44@gmail.com>
2026-01-16 23:14:46 +0530

    forces all buffered file system data to be written to permanent storage

---
+++ System_Calls/File_Subsystem_System_Calls/sync_1.c
@@ -0,0 +1,33 @@
+#include<unistd.h>
+#include<stdio.h>
+#include<fcntl.h>
+#include<errno.h>
+#include<string.h>
+
+int main()
+{
+    int fd = 0;
+    char Buffer[] = "indiaismycountry";
+    int iRet = 0;
+
+    fd = open("Demo.txt",O_CREAT | O_WRONLY | O_APPEND, 0777);
+
+    if(fd < 0)
+    {
+        printf("Unable to open the file \n");
+        printf("Reson : %s\n",strerror(errno));
+
+        return -1;
+
+    }
+
+    printf("File successfully opend with : %d\n",fd);
+
+    iRet = write(fd, Buffer, 16);                       // Over write issue
+
+    printf("%d bytes successfully written\n",iRet);
+
+    close(fd);
+
+    return 0;
+}
